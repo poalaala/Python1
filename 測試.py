61ae95e5928b92ee1cd0fac85f294612643156df
poalaala <guokehong123456@gmail.com>
2026-04-24 15:58:35 +0800

    Delete 測試.py

--- 測試.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-money=[55959,222525,225292,228854,151524]
-type=["1號","2號","3號","4號","5號"]
-plt.rcParams["font.family"]="Microsoft JhengHei"
-plt.bar(type,money,color="b")
-plt.title("我亂做的")
-plt.grid(True)
-plt.show()
